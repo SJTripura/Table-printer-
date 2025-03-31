bd8a70fe1a2e1ca99b80c84d86f59f2b25c5d123
SJTripura <117557618+SJTripura@users.noreply.github.com>
2025-03-31 07:35:20 +0530

    Add files via upload

---
+++ Table printer For loop.py
@@ -0,0 +1,9 @@
+M= int( input ("Enter Tablet number. "))
+N= int (input("Enter Length of table.  "))
+A=N*M#length selector 100
+B=A+1# Length+1=101
+a=0
+for e in range (0,B,M):
+	print (M,"x",a,"=",e,"\n")
+	a=a+1
+	
